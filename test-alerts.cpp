1a718b3788d3546a4b1718d061692396d1b3b732
Amrose <amrose215@gmail.com>
2022-03-30 19:36:37 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -8,5 +8,5 @@ TEST_CASE("infers the breach according to limits") {
 }
 
 TEST_CASE("checkAndAlert_function") {
-  REQUIRE(checkAndAlert(0, 0, 0));
+  REQUIRE(checkAndAlert(TO_CONTROLLER, PASSIVE_COOLING, 0));
 }
